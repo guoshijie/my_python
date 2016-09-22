cdd407fa27f6b01a88fc4baad0c0fd0d8c3da7cc
guoshijie <gsj_java@163.com>
2016-09-22 10:26:16 +0800

    add auto_meican.py

--- simulatiion/simulation_mouse.py
+++ simulatiion/simulation_mouse.py
@@ -23,17 +23,17 @@ browser.find_element_by_id('kw').send_keys(u'郭世杰')
 print browser.find_element_by_id('kw').get_attribute('type')
 print browser.find_element_by_id('kw').size #打印输入框的大小
 browser.find_element_by_id('su').click()
-time.sleep(3)
+time.sleep(1)
 
 print '现在我将设置浏览器为宽480，高800显示'
 browser.set_window_size(480,800)
 browser.get('http://m.mail.10086.cn')
-time.sleep(3)
+time.sleep(1)
 
 print '现在我将回到刚才的页面'
 browser.maximize_window()
 browser.back()
-time.sleep(3)
+time.sleep(1)
 
 # print '现在我将回到之前的页面'
 # browser.forward()
@@ -52,25 +52,26 @@ browser.get('http://www.renren.com/SysHome.do')
 browser.find_element_by_id('email').clear()  # 这个是以id选择元素
 browser.find_element_by_id('email').send_keys('guoshijie_hi@yeah.net')
 browser.find_element_by_id('email').send_keys(Keys.BACK_SPACE)
-time.sleep(2)
+time.sleep(1)
 browser.find_element_by_id('email').send_keys('t')
-s(2)
+s(1)
 browser.find_element_by_id('email').send_keys(Keys.CONTROL, 'a')  # 全选
-s(2)
+s(1)
 browser.find_element_by_id('email').send_keys(Keys.CONTROL, 'x')  # 剪切掉里面的内容
-s(2)
+s(1)
 browser.find_element_by_id('email').send_keys(Keys.CONTROL, 'v')  # 重新输入进去
-s(2)
+s(1)
 browser.find_element_by_name('password').clear()  # 这个是以name选择元素
 browser.find_element_by_name('password').send_keys('guoyanjie')
 # browser.find_element_by_xpath(".//*[@id='login']").click()#这个是以xpath选择元素
 browser.find_element_by_xpath(".//*[@id='login']").send_keys(Keys.ENTER)  # 这里通过点击Enter键来登录
 browser.maximize_window()
+s(1)
 article = browser.find_element_by_link_text(u'郭鹏波')
 ActionChains(browser).move_to_element(article).perform()  # 将鼠标移动到这里，但是这里不好用
 ActionChains(browser).click(article).perform()
 # ActionChains(browser).context_click(article).perform()
-time.sleep(5)
+time.sleep(3)
 
 browser.quit()
 
